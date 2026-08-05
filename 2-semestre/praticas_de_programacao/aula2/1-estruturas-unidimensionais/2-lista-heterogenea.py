8b458d7e4f1bc8e6c6b8b2cb619c99149ab81c8c
A Laura Carlotta <alauracarlotta@gmail.com>
2026-08-05 15:08:52 -0300

    feat(/aula2): add aula estruturas unidimensionais

---
+++ 2-semestre/praticas_de_programacao/aula2/1-estruturas-unidimensionais/2-lista-heterogenea.py
@@ -0,0 +1,5 @@
+# Lista heterogênea com diferentes tipos de dados
+dados = ["sol", 32, 2.4, "lua", 64, True, -1.2]
+# Acessar o quarto elemento (índice 3)
+print("Lista heterogênea:", dados)
+print("Quarto elemento:", dados[3])
