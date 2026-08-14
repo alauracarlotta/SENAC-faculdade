4cd1aa655d07cc77f3b3c503b51ebd59e48ee6b2
A Laura Carlotta <alauracarlotta@gmail.com>
2026-08-13 22:24:50 -0300

    feat(wip): add `calculate_total_stock` function

--- _TECH/2-semestre/praticas_de_programacao/PTI/desenvolvimento-de-um-sistema-de-controle-de-estoque/index.py
+++ _TECH/2-semestre/praticas_de_programacao/PTI/desenvolvimento-de-um-sistema-de-controle-de-estoque/index.py
@@ -1,6 +1,7 @@
 import time
-# stock = []
-inventory = []
+
+# Lista principal para armazenar os dicionários dos produtos
+stock = []
 
 """ new_product = {
 	"codigo": int(input('Informe o código de barras do produto: ')),
@@ -9,35 +10,62 @@ inventory = []
 	"quantidade": int(input('Informe a quantidade de itens do produto: '))
 } """
 
+""" new_product = {
+		"codigo": 123,
+		"nome": 'test1',
+		"preco": 50.00,
+		"quantidade": 10
+	}
+
+    stock.append(new_product) """
+
+# ok
 def display_menu() :
-	print('============= MENU =============')
+	"""Exibe o menu de opções e retorna a escolha do usuário."""
+	print('================== MENU =================')
 	print('1 - Cadastrar Produto')
 	print('2 - Calcular Total de Produtos em Estoque')
 	print('0 - SAIR')
-	print('================================')
+	print('=========================================')
 	try:
 		value_option = int(input('Informe a opção desejada: '))
 		return value_option
 	except ValueError:
 		return -1
 
-
+# ok
 def incorrect_option():
 	print("\n⚠️  OPÇÃO INVÁLIDA ou DIGITAÇÃO INCORRETA.")
-	print("Por favor, escolha uma das opções válidas do menu.")
+	print("Por favor, escolha uma das opções válidas do menu.\n")
 
-
-def register_product():
-    """Função que será responsável pelo cadastro e validações."""
-    print("\n--- Cadastrar Produto ---")
-    # TODO: Implementar lógica de cadastro, validação de código duplicado
-    # e validação de preço/quantidade não negativos!
-
-
+# ok
 def calculate_total_stock():
     """Função que fará a soma de todas as quantidades."""
     print("\n--- Total de Produtos em Estoque ---")
-    # TODO: Implementar a soma das quantidades dos itens no 'inventory'
+    stock_total = sum(map(lambda x: x["quantidade"], stock))
+    return stock_total
+
+
+# TODO: Implementar lógica de cadastro, validação de código duplicado
+# e validação de preço/quantidade não negativos!
+def register_product():
+    """Função que será responsável pelo cadastro e validações."""
+    print("\n--- Cadastrar Produto ---")
+
+
+# TODO: Implementar lógica de validação de código duplicado
+def code_product(code):
+	return code
+
+
+# TODO: Implementar lógica de validação de preço não negativos!
+def price_product(price):
+	return price
+
+
+# TODO: Implementar lógica de validação de quantidade não negativos!
+def quantity_product(quantity):
+	return quantity
 
 
 while True:
@@ -45,13 +73,16 @@ while True:
 	if value_menu == 1:
 		print('cadastrar produto')
 		register_product()
-	
+
+	# ok
 	elif value_menu == 2:
-		print('o estoque é')
-		value_menu = display_menu()
-	
+		print(f'Valor total: {calculate_total_stock()} itens. \n')
+
+	# ok
 	elif value_menu == 0:
 		break
+	
+	# ok
 	else:
 		incorrect_option()
 
