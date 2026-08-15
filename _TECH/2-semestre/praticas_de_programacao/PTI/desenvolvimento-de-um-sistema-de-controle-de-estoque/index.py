d79dde4d7745e06bb7bb0b47e15c06b9f9a5e198
A Laura Carlotta <alauracarlotta@gmail.com>
2026-08-15 17:35:57 -0300

    refactor: rename variables and apply final validation refinements

--- _TECH/2-semestre/praticas_de_programacao/PTI/desenvolvimento-de-um-sistema-de-controle-de-estoque/index.py
+++ _TECH/2-semestre/praticas_de_programacao/PTI/desenvolvimento-de-um-sistema-de-controle-de-estoque/index.py
@@ -6,12 +6,15 @@ utilizando uma interface baseada em terminal (CLI).
 """
 
 
+from tabulate import tabulate
 import regex
+import time
 
-#? [ ] Remover prints
-#? [ ] Remover comentários
-#? [ ] Verificar 'docstrings' de cada função.
-#? [ ] Add README.md 
+
+# Formatação de Texto / Estilos ANSI do Terminal
+ITALIC = "\033[3m"
+BOLD = "\033[1m"
+RESET = "\033[0m"
 
 # Lista principal para armazenar os dicionários dos produtos
 stock = []
@@ -20,12 +23,14 @@ stock = []
 error_message = {
 	"menu_error": "⚠️  Por favor, escolha uma das opções válidas do menu.\n",
     "calculate_total_stock_error_message": "Não há produtos cadastrados.\n",
-	"code_error_product": "⚠️  Informe um código válido para o produto.\n",
-	"name_error_product": "⚠️  Informe corretamente o nome do produto.\n",
+	"code_error_product": "⚠️  Informe um código válido para o produto (deverá ter até 13 números).\n",
+	"name_error_product": "⚠️  Informe corretamente o nome do produto (deverá ter pelo menos 3 letras).\n",
 	"price_error_product": "⚠️  Informe o valor correto do produto.\n",
 	"quantity_error_product": "⚠️  Informe a quantidade correta do produto.\n",
 }
 
+
+def incorrect_option(code_error: str) -> None:
 	"""Exibe no terminal uma mensagem padronizada para entradas e opções inválidas.
 
     Args:
@@ -35,6 +40,7 @@ error_message = {
 	print(code_error)
 
 
+def display_menu() -> int :
 	"""Exibe o menu principal de navegação e captura a escolha do usuário.
 
     Returns:
@@ -53,7 +59,7 @@ error_message = {
 		return -1
 
 
-#! TODO: Padronizar variáveis? pt ou en?
+def register_product() -> None:
     """Gerencia o fluxo completo de cadastro de um novo produto no estoque.
 
     Solicita e valida código, nome, preço e quantidade antes de armazenar
@@ -64,8 +70,7 @@ error_message = {
     name = get_product_name()
     price = get_product_price()
     quantity = get_product_quantity()
-    
-    # TODO: verificar a ebição do está cadastrado.
+
     new_product = {
 		"code": code,
 		"name": name,
@@ -74,6 +79,10 @@ error_message = {
 	}
 
     stock.append(new_product)
+    print(f'ITEM: \n | Código: {code} || Nome: {name} || Preço: {price} || Quantidade: {quantity} |\n')
+    print('...cadastrando produto...')
+    time.sleep(2)
+    print(f'✅  {ITALIC}PRODUTO CADASTRADO COM SUCESSO!!!{RESET}\n')
 
 
 def display_stock() -> None:
@@ -101,6 +110,7 @@ def display_stock() -> None:
 	print("=====================================================\n")
 
 
+def calculate_total_stock() -> None:
     """Calcula e exibe a quantidade total somada de itens presentes no estoque via função lambda.
 
     Soma as quantidades de todos os produtos cadastrados e exibe o resultado.
@@ -108,13 +118,14 @@ def display_stock() -> None:
     print("\n--- Total de Produtos em Estoque ---")
 
     if len(stock) > 0:
-        stock_total = sum(map(lambda x: x["quantidade"], stock))
+        stock_total = sum(map(lambda x: x["quantity"], stock))
         print(f'Valor total: {stock_total} itens. \n')
         return
     else:
         print(error_message["calculate_total_stock_error_message"])
 
 
+def get_product_code() -> int:
     """Solicita e valida o código de barras do produto.
 
     Garante que a entrada contenha apenas dígitos (até 13 caracteres),
@@ -125,15 +136,29 @@ def display_stock() -> None:
         int: Código de barras validado e convertido para número inteiro.
     """
 
-			if new_code < 0:
-				incorrect_option(error_message["code_error_product"])
-			elif not new_code in list(map(lambda x: x['codigo'], stock)):
-				return new_code
-			else:
-				print(f'\nO código {new_code} é de um produto já registrado. Adicione um novo código!\n')
-		except ValueError:
-			incorrect_option(error_message["code_error_product"])
+    while True:
+        try:
+            entry = input('Informe o código de barras do produto: ').strip()
+            print('----------------------------------------')
 
+            if not entry.isdigit() or len(entry) > 13:
+                incorrect_option(error_message["code_error_product"])
+                continue
+
+            formatted_entry = entry.ljust(13, '0')
+
+            new_code = int(formatted_entry)
+
+            if not new_code in list(map(lambda x: x['code'], stock)):
+                return new_code
+            else:
+                print(f'\nO código {formatted_entry} é de um produto já registrado. Adicione um novo código!\n')
+
+        except ValueError:
+            incorrect_option(error_message["code_error_product"])
+
+
+def get_product_name() -> str:
 	"""Solicita e valida o nome do produto através de expressões regulares (Regex).
 
     Exige no mínimo 3 caracteres no total e pelo menos 3 letras (suportando Unicode/acentuação).
@@ -142,20 +167,21 @@ def display_stock() -> None:
         str: Nome do produto validado.
     """
 
-	valid_name_regex = r"^(?=(?:.*\p{L}){2,}).{3,}$"
+	valid_name_regex = r"^(?=(?:.*\p{L}){3,}).{3,}$"
 	while True:
 		try:
-			new_name = input('Informe o nome do produto: ')
+			new_name = input('Informe o nome do produto: ').strip()
+			print('----------------------------------------')
+
 			if regex.search(valid_name_regex, new_name):
 				return new_name
 			else:
-				print('else name')
 				incorrect_option(error_message["name_error_product"])
 		except ValueError:
-			print('except')
 			incorrect_option(error_message["name_error_product"])
 
 
+def get_product_price() -> str:
 	"""Solicita e valida o preço unitário do produto.
 
     Garante que o valor digitado não seja negativo e o converte para o formato
@@ -164,15 +190,17 @@ def display_stock() -> None:
     Returns:
         str: Preço formatado em string como moeda brasileira.
     """
+
 	while True:
 		try:
-			# Troca vírgula por ponto para aceitar entradas como 10,50
 			entry = input('Informe o preço do produto: R$ ').strip().replace(',', '.')
+			print('----------------------------------------')
+
 			new_price = float(entry)
 			
 			if new_price < 0:
 				incorrect_option(error_message["price_error_product"])
-				continue # Volta para o início do loop em caso de preço negativo
+				continue
 
 			return f"R$ {new_price:.2f}".replace('.', ',')
 		except ValueError:
@@ -205,18 +233,17 @@ def get_product_quantity() -> int:
 
 # Loop principal de execução do menu
 while True:
-	value_menu = display_menu()
-	if value_menu == 1:
+	menu_option = display_menu()
+	if menu_option == 1:
 		register_product()
 
-# TODO: Adicionar Tabela de apresentação de estoque
-	elif value_menu == 2:
+	elif menu_option == 2:
 		display_stock()
 
-	elif value_menu == 3:
+	elif menu_option == 3:
 		calculate_total_stock()
 
-	elif value_menu == 0:
+	elif menu_option == 0:
 		break
 
 	else:
